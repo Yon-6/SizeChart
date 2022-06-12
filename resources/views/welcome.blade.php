3d6e1c1566d150445865b7e3435d482cb2047243
Yon-6 <dynabook.ex63@gmail.com>
2022-06-12 17:00:32 +0900

    環境構築

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="asset/css/reset.css">
+        <link rel="stylesheet" href="asset/css/style.css">
+    </head>
+    <body class="antialiased">
+
+    </body>
+</html>
